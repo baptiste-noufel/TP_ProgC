724777082516f4caae3aed4fe5747ebf4776afba
Baptiste <152295528+baptiste-noufel@users.noreply.github.com>
2026-09-18 09:10:09 +0000

    Exercice 1.10 : Pyramide de nombres avec boucles imbriquées

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+int main(void) {
+    int n = 5;
+    int i, j;
+
+    for (i = 1; i <= n; i++) {
+        /* Espaces pour centrer la pyramide */
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+        /* Nombres croissants de 1 à i */
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        /* Nombres décroissants de i - 1 à 1 */
+        for (j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    printf("Génération de la pyramide terminée.\n");
+
+    return 0;
+}
